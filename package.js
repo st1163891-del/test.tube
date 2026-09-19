b69535afece6128bfd9a5885344940b3fceaaa4a
st1163891-del <st1163891@gmail.com>
2026-09-19 10:45:55 +0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "roblox-youtube-video",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^5.1.0",
+    "youtube-dl-exec": "^3.0.20"
+  }
+}
